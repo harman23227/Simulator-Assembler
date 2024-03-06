7c39ae1fd9706fc595dbb8f1828f82c05d5acf45
harman23227 <harman23227@iiitd.ac.in>
2024-03-07 01:27:59 +0530

    added a bunch of things and opening input file

--- assembler.py
+++ assembler.py
@@ -1,38 +1,67 @@
 def binary_string(number):
     return bin(number)[2:]
+def decimal_to_binary_twos_complement(decimal_number):
+    if decimal_number < 0:
+        binary_string = bin(decimal_number & int("1" * (decimal_number.bit_length() + 1), 2))[2:]
+    else:
+        binary_string = bin(decimal_number)[2:]
+
+    return binary_string
+
+def fill(binary_string, num_bits,s):
+    if len(binary_string) < num_bits:
+        binary_string = s * (num_bits - len(binary_string)) + binary_string
+    
+    elif len(binary_string) > num_bits:
+        binary_string = binary_string[-num_bits:]
+    
+    return binary_string
+
+
 
 r_type_instructions = {
-    "add": {"opcode": "0110011", "funct3": "000", "funct7": "0000000"},
-    "sub": {"opcode": "0110011", "funct3": "000", "funct7": "0100000"},
-    "sll": {"opcode": "0110011", "funct3": "001", "funct7": "0000000"},
-    "slt": {"opcode": "0110011", "funct3": "010", "funct7": "0000000"},
-    "sltu": {"opcode": "0110011", "funct3": "011", "funct7": "0000000"},
-    "xor": {"opcode": "0110011", "funct3": "100", "funct7": "0000000"},
-    "srl": {"opcode": "0110011", "funct3": "101", "funct7": "0000000"},
-    "or": {"opcode": "0110011", "funct3": "110", "funct7": "0000000"},
-    "and": {"opcode": "0110011", "funct3": "111", "funct7": "0000000"}
+    "add": {"opcode": "0110011", "rd": "", "funct3": "000", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "sub": {"opcode": "0110011", "rd": "", "funct3": "000", "rs1": "", "rs2": "", "funct7": "0100000"},
+    "sll": {"opcode": "0110011", "rd": "", "funct3": "001", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "slt": {"opcode": "0110011", "rd": "", "funct3": "010", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "sltu": {"opcode": "0110011", "rd": "", "funct3": "011", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "xor": {"opcode": "0110011", "rd": "", "funct3": "100", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "srl": {"opcode": "0110011", "rd": "", "funct3": "101", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "or": {"opcode": "0110011", "rd": "", "funct3": "110", "rs1": "", "rs2": "", "funct7": "0000000"},
+    "and": {"opcode": "0110011", "rd": "", "funct3": "111", "rs1": "", "rs2": "", "funct7": "0000000"}
 }
+
 i_type_instructions = {
-    "lw": {"opcode": "0000011", "funct3": "010"},
-    "addi": {"opcode": "0010011", "funct3": "000"},
-    "sltiu": {"opcode": "0010011", "funct3": "011"},
-    "jalr": {"opcode": "1100111", "funct3": "000"}
+    "lw": {"opcode": "0000011","rd": "", "funct3": "010", "rs1": "",  "imm": ""},
+    "addi": {"opcode": "0010011","rd": "", "funct3": "000", "rs1": "",  "imm": ""},
+    "sltiu": {"opcode": "0010011","rd": "", "funct3": "011", "rs1": "", "imm": ""},
+    "jalr": {"opcode": "1100111","rd": "", "funct3": "000", "rs1": "",  "imm": ""}
 }
+
 s_type_instructions = {
-    "sw": {"opcode": "0100011", "funct3": "010"},
+    "sw": {"opcode": "0100011","imm":"", "funct3": "010","rs1":"","rs2":""},
 }
 b_type_instructions = {
-    "beq": {"opcode": "1100011", "funct3": "000"},
-    "bne": {"opcode": "1100011", "funct3": "001"},
-    "blt": {"opcode": "1100011", "funct3": "100"},
-    "bge": {"opcode": "1100011", "funct3": "101"},
-    "bltu": {"opcode": "1100011", "funct3": "110"},
-    "bgeu": {"opcode": "1100011", "funct3": "111"},
+    "beq": {"opcode": "1100011", "imm1": "", "funct3": "000", "rs1": "", "rs2": "", "imm2": ""},
+    "bne": {"opcode": "1100011", "imm1": "", "funct3": "001", "rs1": "", "rs2": "", "imm2": ""},
+    "blt": {"opcode": "1100011", "imm1": "", "funct3": "100", "rs1": "", "rs2": "", "imm2": ""},
+    "bge": {"opcode": "1100011", "imm1": "", "funct3": "101", "rs1": "", "rs2": "", "imm2": ""},
+    "bltu": {"opcode": "1100011", "imm1": "", "funct3": "110", "rs1": "", "rs2": "", "imm2": ""},
+    "bgeu": {"opcode": "1100011", "imm1": "", "funct3": "111", "rs1": "", "rs2": "", "imm2": ""}
 }
+
 u_type_instructions = {
-    "lui": {"opcode": "0110111"},
-    "auipc": {"opcode": "0010111"},
+    "lui": {"opcode": "0110111","rd":"","imm":""},
+    "auipc": {"opcode": "0010111","rd":"","imm":""},
 }
 j_type_instructions = {
-    "jal": {"opcode": "1101111"},
+    "jal": {"opcode": "1101111","rd":"","imm":""},
 }
+
+ip=[]
+with open("input.txt", 'r') as file:
+    lines = (line.strip() for line in file if line.strip() and not line.startswith('#'))
+    ip = list(lines)
+johoikl
+
+ 
