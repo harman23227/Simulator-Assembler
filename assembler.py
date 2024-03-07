cc805eef71bdfde449a040a015f4525459189dee
hxybrid3110 <aarush23011@iiitd.ac.in>
2024-03-07 22:41:55 +0530

    register encoding

--- assembler.py
+++ assembler.py
@@ -1,8 +1,8 @@
-def binary_string(number):
-    return bin(number)[2:]
+def binary_string(number):                 #converting decimal to binary string
+    return bin(number)[2:]              
 def decimal_to_binary_twos_complement(decimal_number):
     if decimal_number < 0:
-        binary_string = bin(decimal_number & int("1" * (decimal_number.bit_length() + 1), 2))[2:]
+        binary_string = bin(decimal_number & int("1" * (decimal_number.bit_length() + 1), 2))[2:]   #decimal to 2s complement 
     else:
         binary_string = bin(decimal_number)[2:]
 
@@ -10,7 +10,7 @@ def decimal_to_binary_twos_complement(decimal_number):
 
 def fill(binary_string, num_bits,s):
     if len(binary_string) < num_bits:
-        binary_string = s * (num_bits - len(binary_string)) + binary_string
+        binary_string = s * (num_bits - len(binary_string)) + binary_string   #filling
     
     elif len(binary_string) > num_bits:
         binary_string = binary_string[-num_bits:]
@@ -58,10 +58,47 @@ j_type_instructions = {
     "jal": {"opcode": "1101111","rd":"","imm":""},
 }
 
+register_encoding = {
+    "x0 (zero)": "00000",
+    "x1 (ra)": "00001",
+    "x2 (sp)": "00010",
+    "x3 (gp)": "00011",
+    "x4 (tp)": "00100",
+    "x5 (t0)": "00101",
+    "x6 (t1)": "00110",
+    "x7 (t2)": "00111",
+    "x8 (s0)": "01000",
+    "x9 (s1)": "01001",
+    "x10 (a0)": "01010",
+    "x11 (a1)": "01011",
+    "x12 (a2)": "01100",
+    "x13 (a3)": "01101",
+    "x14 (a4)": "01110",
+    "x15 (a5)": "01111",
+    "x16 (a6)": "10000",
+    "x17 (a7)": "10001",
+    "x18 (s2)": "10010",
+    "x19 (s3)": "10011",
+    "x20 (s4)": "10100",
+    "x21 (s5)": "10101",
+    "x22 (s6)": "10110",
+    "x23 (s7)": "10111",
+    "x24 (s8)": "11000",
+    "x25 (s9)": "11001",
+    "x26 (s10)": "11010",
+    "x27 (s11)": "11011",
+    "x28 (t3)": "11100",
+    "x29 (t4)": "11101",
+    "x30 (t5)": "11110",
+    "x31 (t6)": "11111"
+}
+
 ip=[]
 with open("input.txt", 'r') as file:
-    lines = (line.strip() for line in file if line.strip() and not line.startswith('#'))   #removing uneccesary spaces and empty whitespaces plus comments
+    lines = (line.strip() for line in file.read() if line.strip() and not line.startswith('#'))   #removing uneccesary spaces and empty whitespaces plus comments
     ip = list(lines)   #storing all lines in a list
+
+
 
     
 
