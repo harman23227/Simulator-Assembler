511e0dc2012d56e9fc997872e698e035eb240d80
harman23227 <harman23227@iiitd.ac.in>
2024-03-10 00:02:14 +0530

    perfect version for label handling ,except early program break

--- assembler.py
+++ assembler.py
@@ -201,15 +201,13 @@ register_encoding = {
 
 ip=[]
 with open("input.txt", 'r') as file:
-    lines = (line.strip() for line in file if line.strip() and not line.startswith('#'))   #removing uneccesary spaces and empty whitespaces plus comments
-    ip = list(lines)   #storing all lines in a list
-with open("input.txt", 'r') as file:
-    lines = file.readlines()
-    empty_lines = sum(1 for line in lines if line.strip() == '')
+    ip = [line.strip() for line in file]
 
+
+ip = [line.split('#')[0].strip() for line in ip]
                         
     
-def Ibinaryrep(decimal,totalbits):
+def binaryrep(decimal,totalbits):
     if decimal== 0:
         return '0'.zfill(totalbits)
     if decimal<0:
@@ -226,18 +224,18 @@ def Ibinaryrep(decimal,totalbits):
 
 
 
-def instruction(l,line_no,label):
+def instruction(l,line_no,label,extra):
     if l[0] in r_type_instructions.keys():
         bintemp=""
-        registers=[reg.strip(",") for reg in l[1:]]
-        bintemp+=r_type_instructions[l[0]]["funct7"]
-        bintemp+=register_encoding[registers[0].split(",")[2]]                         
-        bintemp+=register_encoding[registers[0].split(",")[1]]
-        bintemp+=r_type_instructions[l[0]]["funct3"]
-        bintemp+=register_encoding[registers[0].split(",")[0]] 
-        bintemp+=r_type_instructions[l[0]]["opcode"]
+        registers=[reg.strip(",") for reg in list[1:]]
+        bintemp+=r_type_instructions[list[0]]["funct7"]
+        bintemp+=register_encoding[registers[2]]                          #func for rtype , incomplete
+        bintemp+=register_encoding[registers[1]]
+        bintemp+=r_type_instructions[list[0]]["funct3"]
+        bintemp+=register_encoding[registers[0]] 
+        bintemp+=r_type_instructions[list[0]]["opcode"]
     
-    elif l[0] in i_type_instructions.keys():
+    if list[0] in i_type_instructions.keys():
         bintemp=""
         if l[0]==("lw" or "lh" or "lb" or "ld"):
             bintemp +=Ibinaryrep(l[1].split(",")[1].split("(")[0],12)
@@ -253,7 +251,7 @@ def instruction(l,line_no,label):
             bintemp +=register_encoding[registers[0].split(",")[0]]
             bintemp +=i_type_instructions[l[0]]["opcode"]
         
-    elif l[0] in s_type_instructions.keys():
+    elif list[0] in s_type_instructions.keys():
         bintemp=""
         x=Ibinaryrep(l[1].split(",")[1].split("(")[0],12)
         bintemp +=x[11:5]
@@ -266,24 +264,25 @@ def instruction(l,line_no,label):
 
 
 
-    elif l[0] in u_type_instructions.keys():
+    elif list[0] in u_type_instructions.keys():
         bintemp=""
-        registers=[operand.strip(",")for operand in l[1:]]
+        registers=[operand.strip(",")for operand in list[1:]]
         
 
-        bintemp +=Ibinaryrep(immediate,20)
+        bintemp +=binaryrep(immediate,20)
         bintemp +=register_encoding[registers[0]]
-        bintemp +=u_type_instructions[l[0]]["opcode"]
+        bintemp +=u_type_instructions[list[0]]["opcode"]
 
-    elif l[0] in j_type_instructions.keys():
+    if list[0] in j_type_instructions.keys():
         bintemp=""
-        registers=[operand.strip(",")for operand in l[1:]]
+        registers=[operand.strip(",")for operand in list[1:]]
         
-        bintemp +=Ibinaryrep(immediate,20)
+        bintemp +=binaryrep(immediate,20)
         bintemp +=register_encoding[registers[0]]
-        bintemp +=j_type_instructions[l[0]]["opcode"]
+        bintemp +=j_type_instructions[list[0]]["opcode"]
         
-    
+    elif list[0] in s_type_instructions.keys():
+        bintemp=""
         
 
 
@@ -297,36 +296,35 @@ label=dict() #to store appropriate pointers to respective labels
 line_no=0
 for line in ip:
     line_no = line_no+1 
-    l=line.split()
-    m=l[0]
-    for i in instruction_types:
-        if m in i:
-            continue
+    if len(line)==0:
+        continue
     else:
-        nub=line_no +empty_lines
-        d = get_get(line)
-        if d in label:
-            g=open("output.txt","w")
-            g.write(f"Error in Line {nub} ,Label already mentioned" )#mere lode pe
-            g.close()
-            break
-        if d == None:
-            g=open("output.txt","w")
-            g.write(f"Error in Line {nub} ,Invalid Label/Expression" )#mere lode pe
-            g.close()
-            break
+        l=line.split()
+        m=l[0]
+        for i in instruction_types:
+            if m in i:
+                continue
         else:
-            pointer = line_no - 1
-            gray = len(d) + 1
-            line = line[gray:]
-            if len(line) == 0:
-               label[d] = (pointer + 1)*4
-               continue
+            d = get_get(line)
+            if d in label:
+                g=open("output.txt","w")
+                g.write(f"Error in Line {line_no} ,Duplicate Label" )#mere lode pe
+                g.close()
+                break
+            elif d == None:
+                g=open("output.txt","w")
+                g.write(f"Error in Line {line_no} ,Invalid Expression" )#mere lode pe
+                g.close()
+                break
             else:
-               line = line.strip()
-               l=line.split()
-               label[d] = (pointer)*4
-               continue
+                gray = len(d) + 1
+                line = line[gray:]
+                if len(line) == 0:
+                    label[d] = (line_no)*4
+                    continue
+                else:
+                    label[d] = (line_no-1)*4
+                    continue
        
 line_no=0
 for line in ip:
@@ -354,3 +352,4 @@ for line in ip:
                     l=line.split()
                     instruction(l,line_no,label)
                     continue
+            
